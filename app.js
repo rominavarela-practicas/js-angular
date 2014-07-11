61bc134e847730e93001b36827934e1c2e4d7710
rominavarela <rominavarela.mx@gmail.com>
2014-07-11 10:30:25 -0500

    module one, good!

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-var app= angular.module('store',[]);
+(function(){
+  var app= angular.module('gemStore',[]);
+
+  app.controller('StoreController',function(){
+    this.products=gems;
+  });
+
+  var gems=
+  [
+    {
+      name:'Dodecahedron',
+      price:2.95,
+      description:'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of this gems.',
+      canPurchase:true,
+      soldOut:false
+    },
+    {
+      name:'Pentagonal Gem',
+      price:5.95,
+      description:'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of this gems.',
+      canPurchase:true,
+      soldOut:false
+    },
+    {
+      name:'Gemy',
+      price:5.95,
+      description:'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of this gems.',
+      canPurchase:true,
+      soldOut:true
+    }
+  ];
+
+})();
